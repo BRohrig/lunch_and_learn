bee3447b64d4d2c3848c1b24f4fb6236b338d9d2
Brady Rohrig <brady.rohrig@gmail.com>
2023-03-04 15:18:02 -0800

    test: wrote test for attributes index for user

--- spec/requests/api/v1/favorite_request_spec.rb
+++ spec/requests/api/v1/favorite_request_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe "favoriting a recipe endpoint" do
+RSpec.describe 'favorite recipes endpoint' do
   before(:each) do
-    @user = User.create(name: "William McTest", email: "muffinman@drury_lane.com", api_key: "1234567890")
+    @user = User.create(name: 'William McTest', email: 'muffinman@drury_lane.com', api_key: '1234567890')
   end
   it 'can receive a json body with country, recipe title, recipe link, and user api_key and create a favorite for that user' do
     headers = { "Content-Type": 'application/json' }
     body = {  "api_key": "#{@user.api_key}",
               "country": 'Brazil',
-              "recipe_link": "https://www.deliciousmeat.com/STEAK",
-              "recipe_title": "MEAT IS SO TASTY" }
+              "recipe_link": 'https://www.deliciousmeat.com/STEAK',
+              "recipe_title": 'MEAT IS SO TASTY' }
 
     post api_v1_favorites_path, params: body.to_json, headers: headers
 
@@ -19,18 +19,18 @@ RSpec.describe "favoriting a recipe endpoint" do
 
     favorite = Favorite.last
     expect(favorite.user_id).to eq(@user.id)
-    expect(favorite.country).to eq("Brazil")
-    expect(favorite.recipe_link).to eq("https://www.deliciousmeat.com/STEAK")
-    expect(favorite.recipe_title).to eq("MEAT IS SO TASTY")
-    expect(parsed).to eq({"success": "Favorite added successfully"})
+    expect(favorite.country).to eq('Brazil')
+    expect(favorite.recipe_link).to eq('https://www.deliciousmeat.com/STEAK')
+    expect(favorite.recipe_title).to eq('MEAT IS SO TASTY')
+    expect(parsed).to eq({ "success": 'Favorite added successfully' })
   end
 
   it 'returns an error for invalid api_key if the correct one is not given' do
     headers = { "Content-Type": 'application/json' }
-    body = {  "api_key": "I AM A HACKER RAWR",
+    body = {  "api_key": 'I AM A HACKER RAWR',
               "country": 'Brazil',
-              "recipe_link": "https://www.deliciousmeat.com/STEAK",
-              "recipe_title": "MEAT IS SO TASTY" }
+              "recipe_link": 'https://www.deliciousmeat.com/STEAK',
+              "recipe_title": 'MEAT IS SO TASTY' }
 
     post api_v1_favorites_path, params: body.to_json, headers: headers
     expect(response).to_not be_successful
@@ -39,9 +39,31 @@ RSpec.describe "favoriting a recipe endpoint" do
     parsed = JSON.parse(response.body, symbolize_names: true)
 
     expect(parsed[:errors]).to eq('Invalid API key')
-
-
   end
 
+  it 'can list all the favorites associated with a user' do
+    user = create(:user)
+    favorite = create(:favorite)
+    favorite2 = create(:favorite)
+    favorite3 = create(:favorite)
+    favorite4 = create(:favorite)
 
+    get api_v1_favorites_path, params: {api_key: user.api_key}
+    expect(response).to be_successful
+
+    parsed = JSON.parse(response.body, symbolize_names: true)
+
+    expect(parsed).to have_key(:data)
+    expect(parsed[:data]).to be_an(Array)
+    expect(parsed[:data].count).to eq(4)
+    parsed[:data].each do |favorite|
+      expect(favorite).to have_key(:id)
+      expect(favorite[:type]).to eq("favorite")
+      expect(favorite).to have_key(:attributes)
+      expect(favorite[:attributes]).to have_key(:recipe_title)
+      expect(favorite[:attributes]).to have_key(:recipe_link)
+      expect(favorite[:attributes]).to have_key(:country)
+      expect(favorite[:attributes]).to have_key(:created_at)
+    end
+  end
 end
