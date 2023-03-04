89647da8305b5db4c0c441166d283bcaa99d4a60
Brady Rohrig <brady.rohrig@gmail.com>
2023-03-03 17:37:18 -0800

    feat: implemented youtube service call to get a video from mr history

--- spec/services/youtube_service_spec.rb
+++ spec/services/youtube_service_spec.rb
@@ -3,8 +3,9 @@ require 'rails_helper'
 RSpec.describe "youtube api service" do
   it 'can make calls to the youtube API and return a video about a country based on a query', :vcr do
     response = YouTubeService.get_video("Qatar")
-    expect(response[:items][0][:snippet][0]).to be_a(Hash)
-    expect(response[:items][0][:snippet][0][:title]).to eq("A Super Quick History of Qatar")
+    # binding.pry
+    expect(response[:items][0][:snippet]).to be_a(Hash)
+    expect(response[:items][0][:snippet][:title]).to eq("A Super Quick History of Qatar")
     expect(response[:items][0][:id][:videoId]). to eq("eoqBjQKubrQ")
   end
 
