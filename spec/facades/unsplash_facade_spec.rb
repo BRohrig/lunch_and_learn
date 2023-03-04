ab9d1d32bf03dc21644d6b876be8335e5eecfc75
Brady Rohrig <brady.rohrig@gmail.com>
2023-03-03 19:10:52 -0800

    test: wrote test for unsplash facade finding 10 photos of a country and making photo objects out of them

---
+++ spec/facades/unsplash_facade_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe 'unsplash facade' do
+  it 'can find 10 photos related to a country', :vcr do
+    photo_list = UnsplashFacade.get_ten_photos("Peru")
+
+    expect(photo_list).to be_an(Array)
+    photo_list.each do |photo_item|
+      expect(photo_item).to be_a(Photo)
+      expect(photo_item.url).to be_a(String)
+      expect(photo_item.url[0..26]).to eq("https://images.unsplash.com")
+      expect(photo_item.alt_tag).to be_a(String)
+    end
+  end
+
+
+end
