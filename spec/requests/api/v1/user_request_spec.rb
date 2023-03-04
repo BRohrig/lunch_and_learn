14bd9ec8cc8bbb282e21026838d57586fe96ae0b
Brady Rohrig <brady.rohrig@gmail.com>
2023-03-04 10:41:03 -0800

    test: wrote test for sad path, email already in use on user creation

--- spec/requests/api/v1/user_request_spec.rb
+++ spec/requests/api/v1/user_request_spec.rb
@@ -21,5 +21,20 @@ RSpec.describe 'user creation endpoint' do
                                               api_key: user.api_key})
   end
 
+  it 'gives an appropriate error when a user inputs an email that is already in the db' do
+    user = User.create(name: "mr man1", email: "test@test.com", api_key: "2342342341")
+    headers = {"Content-Type": "application/json"}
+    body = {"name": "I am a nice user",
+            "email": "test@test.com"}
+
+    post api_v1_users_path, params: body.to_json, headers: headers
+
+    parsed = JSON.parse(response.body, symbolize_names: true)
+
+    expect(parsed).to have_key(:error)
+    expect(parsed[:error]).to eq("This email is already in use")
+
+  end
+
 
 end
