0b533fae3c21622272999145291607825e9a3f8b
Brady Rohrig <brady.rohrig@gmail.com>
2023-03-03 16:30:06 -0800

    test: wrote test for random country recipe find

--- spec/requests/api/v1/recipes_request_spec.rb
+++ spec/requests/api/v1/recipes_request_spec.rb
@@ -5,9 +5,12 @@ RSpec.describe "recipes requests API V1", :vcr do
     get api_v1_recipes_path, params: {query: "Norway"}
 
     expect(response).to be_successful
+
     parsed_response = JSON.parse(response.body, symbolize_names: true)
+
     expect(parsed_response).to have_key(:data)
     expect(parsed_response[:data]).to be_an(Array)
+    
     parsed_response[:data].each do |recipe|
       expect(recipe).to be_a(Hash)
       expect(recipe).to have_key(:id)
@@ -23,8 +26,29 @@ RSpec.describe "recipes requests API V1", :vcr do
     end
   end
 
-  it 'can pick a random country if none is supplied and return recipes for that country' do
+  it 'can pick a random country if none is supplied and return recipes for that country', :vcr do
     get api_v1_recipes_path
+
+    country_list = CountriesFacade.country_names
+
+    expect(response).to be_successful
+
+    parsed_response = JSON.parse(response.body, symbolize_names: true)
+
+    expect(parsed_response[:data]).to be_an(Array)
+    parsed_response[:data].each do |recipe|
+      expect(recipe).to be_a(Hash)
+      expect(recipe).to have_key(:id)
+      expect(recipe).to have_key(:type)
+      expect(recipe[:type]).to eq("recipe")
+      expect(recipe).to have_key(:attributes)
+      expect(recipe[:attributes]).to be_a(Hash)
+      expect(recipe[:attributes]).to have_key(:title)
+      expect(recipe[:attributes]).to have_key(:url)
+      expect(recipe[:attributes]).to have_key(:country)
+      expect(country_list).to include(recipe[:attributes][:country])
+      expect(recipe[:attributes]).to have_key(:image)
+    end
 
   end
 
