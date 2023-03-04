eb1092d8dcf958c20a6a7a827a74b07ea8b623cd
Brady Rohrig <brady.rohrig@gmail.com>
2023-03-03 16:07:50 -0800

    feat: implemented countries service

--- spec/services/countries_service_spec.rb
+++ spec/services/countries_service_spec.rb
@@ -4,11 +4,10 @@ RSpec.describe 'countries service' do
   it 'can get a list of all countries', :vcr do
     response = CountriesService.get_countries
     expect(response).to be_an(Array)
-    expect(response.count).to eq(187)
+    expect(response.count).to eq(250)
     response.each do |country|
       expect(country).to have_key(:name)
     end
-
   end
 
 
