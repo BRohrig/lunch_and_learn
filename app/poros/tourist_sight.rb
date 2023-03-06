a1c526c7a340972808fa074fd1456c31805b72e4
Brady Rohrig <brady.rohrig@gmail.com>
2023-03-06 09:39:17 -0800

    fixed place_id

--- app/poros/tourist_sight.rb
+++ app/poros/tourist_sight.rb
@@ -4,7 +4,7 @@ class TouristSight
   def initialize(input_data)
     @name = input_data[:properties][:name]
     @address = input_data[:properties][:address_line2]
-    @place_id = input_data[:place_id]
+    @place_id = input_data[:properties][:place_id]
     @id = "null"
   end
 end
